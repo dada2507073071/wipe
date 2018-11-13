f877f88cf8f97af6f73bcd67a3df303f080e65ae
dada2507673071 <1430244950@qq.com>
2018-11-13 10:07:41 +0800

    增加移动端支持

--- src/js/wipe.js
+++ src/js/wipe.js
@@ -1,89 +1,161 @@
 var cas = document.getElementById('cas');
-var context= cas.getContext("2d");
+var context = cas.getContext("2d");
 var _w = cas.width;
 var _h = cas.height;
-var t = 0;
-var radius = 20;
 var moveX = 0;
 var moveY = 0;
-var moveZ = 0;
-var moveW = 0;
-var isMouseDown = false;//表示鼠标的状态，是否按下，默认未按下false，按下为true；
-function drawLine(context){
-	if (isMouseDown) {
-		context.save();
-		context.beginPath();
-		context.lineCap = "round";
-		context.moveTo(moveX,moveY);
-		context.lineWidth=radius*2;
-		context.lineTo(moveZ,moveW);
-		context.stroke();
-		context.restore();
-	}
-}
+var x1 = 0;
+var x2 = 0;
+var y1 = 0;
+var y2 = 0;
+var radius = 20;//涂抹的半径
+var isMouseDown = false;//表示鼠标的状态,是否按下,默认未按下false,按下true
+var device = (/android|webos|iPhone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
+// 判断是手机端还是PC端
+var press = device ? "touchstart" : "mousedown";
+var move = device ? "touchmove" : "mousemove";
+var loosen = device ? "touchend" : "mouseup";
 function drawRect(context){
-	context.fillStyle="#666";
-	context.fill();
+	context.save();
+	context.beginPath();
+	context.fillStyle = "#666";
 	context.fillRect(0,0,_w,_h);
 	context.globalCompositeOperation = "destination-out";
 }
+function drawarv(context,moveX,moveY){
+	console.log("传递实参的个数：" + arguments.length);
+	context.save();
+	context.beginPath();
+	context.arc(moveX,moveY,radius,0,2*Math.PI);
+	context.fillStyle = "red";
+	context.fill();
+	context.restore();
+}
+function drawLine(context, x1, y1, x2, y2){
+	console.log("传递实参的个数：" + arguments.length);
+	context.save();
+	context.lineCap = "round";
+	context.beginPath();
+	context.moveTo(x1,y1);
+	context.lineTo(x2,y2);
+	context.lineWidth = radius*2;
+	context.stroke();
+	context.restore();
+}
+// function drawLine(context,x1,y1,x2,y2,moveX,moveY){
+// 	if (arguments.length === 3) {
+// 		console.log("传递实参的个数：" + arguments.length);
+// 		context.save();
+// 		context.beginPath();
+// 		context.arc(moveX,moveY,radius,0,2*Math.PI);
+// 		context.fillStyle = "red";
+// 		context.fill();
+// 		context.restore();
+// 	}else{
+// 		console.log("传递实参的个数：" + arguments.length);
+// 		context.save();
+// 		context.lineCap = "round";
+// 		context.beginPath();
+// 		context.moveTo(x1,y1);
+// 		context.lineTo(x2,y2);
+// 		context.lineWidth = radius*2;
+// 		context.stroke();
+// 		context.restore();
+// 	}
+// }
 
-// function drawPornt(context,moveX,moveY){
-// 	context.save();
-// 	context.beginPath();
-// 	// context.translate(150,250);
-// 	context.arc(moveX,moveY,radius,0,2*Math.PI);
-// 	context.fillStyle="red";
-// 	context.fill();
-// 	context.restore();
-// };
-	cas.addEventListener("mousedown",function(evt){
+// 在canvas画布上监听自定义事件"mousedown",调用drawPoint函数
+cas.addEventListener(press,function(evt){
+	var event = evt || window.event;
+	//获取鼠标在视口的坐标,传递参数到drawPoint
+	x1 = device ? event.touches[0].clientX : event.clientX;
+	y1 = device ? event.touches[0].clientY : event.clientY;
+	isMouseDown = true;
+	drawarv(context,x1,y1);
+},false);
+cas.addEventListener(move,function(evt){
+	if (!isMouseDown ) {
+		return false;
+	}else{
 		var event = evt || window.event;
-		isMouseDown = true;
-		//获取鼠标在视口的坐标，传递参数到drawPornt
-		moveX = event.clientX;
-		moveY = event.clientY;
-		// drawLine(context,moveX,moveY);
-	},false);
-	cas.addEventListener("mousemove",fn1,false);
-	function fn1(evt){
-			var event = evt || window.event;
-			moveZ = event.clientX;
-			moveW = event.clientY;
-			drawLine(context,moveX,moveY,moveZ,moveW);
-			//每次结束点变成下一次的开始点
-			moveX = moveZ;
-			moveY = moveW;
+		event.preventDefault();
+		x2 = device ? event.touches[0].clientX : event.clientX;
+		y2 = device ? event.touches[0].clientY : event.clientY;
+		//drawPoint(context,a,b);
+		drawLine(context,x1,y1,x2,y2);
+		//每次的结束点变成下一次划线的开始点
+		x1 = x2;
+		y1 = y2;
 	}
-	cas.addEventListener('mouseup',function(evt){
-		// cas.removeEventListener("mouseup",fn1,false);
-		// isMouseDown还原为false
-		isMouseDown = false;
-		if(getTransparencyPercent(context)>50){
-			// alert("超过了50%的面积");
-			alert("这是我曾血拼打下的江山，曾经追她穷如狗，现在看她嫌她丑，扔之可惜，牵之傻逼!!!");
-			// drawRect(context);
-			clearRect(context);
-		}
-	},false);
-	function clearRect(context){
-		context.clearRect(0,0,_w,_h);
-	}
-	function getTransparencyPercent(context){
-		var imgData = context.getImageData(0,0,_w,_h);
-		for(var i=0;i<imgData.data.length;i+=4){
-			var a = imgData.data[i+3];
-			if ( a === 0){
-				t++;
+},false);
+// 手势触摸
+// cas.addEventListener("touchstart",function(evt){
+// 	isMouseDown=true;
+// 	var event = evt || window.event;
+// 	//获取鼠标在视口的坐标,传递参数到drawPoint
+// 	moveX = event.touches[0].clientX;
+// 	moveY = event.touches[0].clientY;
+// 	drawarv(context,moveX,moveY);
+// },false);
+// 手势滑动
+// cas.addEventListener("touchmove",fn1,false);
+// function fn1(evt){
+// 	//判断,当isMouseDown为true时,才执行下面的操作
+// 	if (!isMouseDown ) {
+// 		return false;
+// 	}else{
+// 		var event = evt || window.event;
+// 		event.preventDefault();
+// 		var x2 = event.touches[0].clientX;
+// 		var y2 = event.touches[0].clientY;
+// 		//drawPoint(context,a,b);
+// 		drawLine(context,x1,y1,x2,y2);
+// 		//每次的结束点变成下一次划线的开始点
+// 		x1 = x2;
+// 		y1 = y2;
+// 	}
+// }
+cas.addEventListener(loosen,function(){
+	var c = 0;
+	var a = context.getImageData(0,0,_w,_h);
+		console.log(a);
+	for (var j = 0; j < _h; j++) {
+		for (var i = 0; i < _w; i++) {
+			var f = ((_w*j)+i)*4+3;
+			if (a.data[f] == 0) {	
+				c++;
 			}
-		}	
-		var percent = (t / (_w*_h) )*100;
-		console.log("透明点个数：" + t + "个");
-		console.log("占总面积"+ Math.ceil(percent)+"%");
-		return Math.ceil(percent);
+		}
 	}
-	window.onload = function(){
+	var tmb = c/(_w*_h)*100;
+	console.log(Math.round(tmb));
+	if (tmb > 60) {
+		clearRect(context);
+	}
+	isMouseDown = false;
+},false);
+// cas.addEventListener("touchend",function(){
+// 	var c = 0;
+// 	var a = context.getImageData(0,0,_w,_h);
+// 		console.log(a);
+// 	for (var j = 0; j < _h; j++) {
+// 		for (var i = 0; i < _w; i++) {
+// 			var f = ((_w*j)+i)*4+3;
+// 			if (a.data[f] == 0) {	
+// 				c++;
+// 			}
+// 		}
+// 	}
+// 	var tmb = c/(_w*_h)*100;
+// 	console.log(Math.round(tmb));
+// 	if (tmb > 60) {
+// 		clearRect(context);
+// 	}
+// 	isMouseDown = false;
+// },false);
+function clearRect(context){
+	context.clearRect(0,0,_w,_h);
+}
+window.onload = function(){
 	drawRect(context);
-	// drawPornt(context);
 };
-
